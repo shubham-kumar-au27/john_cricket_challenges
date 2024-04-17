7d0d1f6e9bc765241f14809a91392507a8cd9de2
Shubham kumar <s.kumar.s8101997@gmail.com>
2024-04-17 14:29:47 +0530

    added src

--- urlshortner/backend/src/routes/router.js
+++ urlshortner/backend/src/routes/router.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 //get all url links created--------
 
+router.get('/',(req,res)=>{
+    res.send("hello")
+})
+
 //post or create short url---
 router.post('/urls',(req,res)=>{
     const {redirectUrl} = req.body
