0886c2009c25b5a4cd066d62a5e08814cfc71aa1
Shubham kumar <s.kumar.s8101997@gmail.com>
2024-04-17 15:05:15 +0530

    added .env file

--- urlshortner/backend/src/server.js
+++ urlshortner/backend/src/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/router')
-
+const dotenv = require('dotenv')
+dotenv.config()
 
 
 const app = express()
 
+
 app.use(routes)
 
+const mongourl = 'mongodb+srv://shu810:shu810@cluster0.uub44.mongodb.net/urlshortner?retryWrites=true&w=majority&appName=Cluster0'
+const MONGO_URL = process.env.MONGO_URL
+const PORT = process.env.PORT
 
-app.listen(3000,()=>{
+
+mongoose.connect(MONGO_URL||mongourl).then(()=> console.log("connection to db successful")).catch((err)=> console.log(err,'connection failed'))
+
+
+app.listen(PORT,()=>{
     console.log("Listening at port 3000")
 })
 
-mongoose.connect("mongodb+srv://shu810:shu810@cluster0.uub44.mongodb.net/urlshortner?retryWrites=true&w=majority&appName=Cluster0").then(()=> console.log("connection to db successful")).catch((err)=> console.log('connection failed'))
